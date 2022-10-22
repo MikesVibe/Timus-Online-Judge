a24f64547f7e8d5d8fc6df363777d2273c6af965
MikesVibe <miki889-99@wp.pl>
2022-10-22 09:55:36 +0200

    Adding solution to problem

---
+++ 1293. Eniya/1293. Eniya/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var intigers = input.Split(" ");
+
+            int output = 1;
+            foreach (var intiger in intigers)
+            {
+                output *= Int32.Parse(intiger);
+            }
+            output *= 2;
+            Console.WriteLine(output);
+        }
+    }
+}
